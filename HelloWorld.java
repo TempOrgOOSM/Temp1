a46334a7c3bc457f28d989ab4fe6ce8531c78321
Pratik Jain <pratikjain1104@gmail.com>
2018-02-15 17:30:37 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,21 +2,22 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
+        for(int i = 0;i<5;i++)
         System.out.println("Hello, World");
+        for(int i = 0;i<5;i++)
         System.out.println("Hello, World");
+        for(int i = 0;i<5;i++)
         System.out.println("Hello, World");
+        for(int i = 0;i<5;i++)
         System.out.println("Hello, World");
+        for(int i = 0;i<5;i++)
         System.out.println("Hello, World");
+        for(int i = 0;i<5;i++)
         System.out.println("Hello, World");
+        for(int i = 0;i<5;i++)
         System.out.println("Hello, World");
+        for(int i = 0;i<5;i++)
         System.out.println("Hello, World");
-        int x1 = 5;
-        int x2 = 5;
-        int x3 = 5;
-        int x4 = 5;
-        int x5 = 5;
-        int x6 = 5;
-        int x7 = 5;
     }
 
 }
